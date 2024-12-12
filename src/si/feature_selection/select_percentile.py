0f8519b209c3af9eaac4c71126f76b71f7bd57c0
sofiaferreira02 <sofiaferreira200@gmail.com>
2024-12-12 14:37:29 +0000

    modified

--- src/si/feature_selection/select_percentile.py
+++ src/si/feature_selection/select_percentile.py
@@ -1,70 +1,54 @@
 import numpy as np
 from typing import Callable
+from si.base.transformer import Transformer
 from si.data.dataset import Dataset
 from si.statistics.f_classification import f_classification
 
 
-class SelectPercentile:
+class SelectPercentile(Transformer):
     """
-        Classe que integra os métodos responsáveis pela seleção de um percentil de features segundo a análise da
-        variância(score_func), sendo que o percentil a selecionar é dado pelo utilizador.
+    Select features according to the highest scores up to a certain percentile.
+    Feature ranking is performed by computing the scores of each feature using a scoring function:
+        - f_classification: ANOVA F-value between label/feature for classification tasks.
+        - f_regression: F-value obtained from F-value of r's pearson correlation coefficients for regression tasks.
+
+    Parameters
+    ----------
+    score_func: callable
+        Function taking dataset and returning a pair of arrays (scores, p_values)
+    percentile: float, default=10
+        Percentile of top features to select.
+
+    Attributes
+    ----------
+    F: array, shape (n_features,)
+        F scores of features.
+    p: array, shape (n_features,)
+        p-values of F-scores.
+    """
+
+    def __init__(self, score_func: Callable = f_classification, percentile: float = 10, **kwargs):
+        """
+        Select features according to the highest scores up to a certain percentile
 
         Parameters
         ----------
+
         score_func: callable
-            Function taking dataset and returning a pair of arrays (scores, p_values).
-        percentile: int
-            Percentile value.
-
-        Attributes
-        ----------
-        F: array, shape (n_features,)
-            F scores of features
-        p: array, shape (n_features,)
-            p-values of F-scores
+            Function taking dataset and returning a pair of arrays (scores, p_values)
+        percentile: float, default=10
+            Percentile of top features to select.
+        
         """
-
-    def __init__(self, score_func: Callable = f_classification, percentile: int = 10):
-        """
-        Construtor
-
-        :param score_func: função de análise da variância (f_classification() ou f_regression())
-        :param percentile: valor do percentile. Apenas F-scores acima desse valor permanecem no dataset filtrado
-        """
-        if percentile > 100 or percentile < 0:
-            raise ValueError("Your percentile must be between 0 and 100")
-
-        self.percentile = percentile
+        super().__init__(**kwargs)
         self.score_func = score_func
+        self.percentile = percentile
         self.F = None
         self.p = None
 
-    def transform(self, dataset: Dataset) -> Dataset:
+    def _fit(self, dataset : Dataset) -> 'SelectPercentile':
         """
-        Method that selects the features with the highest F value in the desired percentile
-
-        Parameters
-        ----------
-        dataset: Dataset, input Dataset
-
-
-        Returns
-        -------
-        dataset: Dataset
-            A labeled dataset with the select features.
-        """
-        len_features = len(dataset.features)
-        features_percentile = int(
-            len_features * (self.percentile / 100)  # calcula o nº de features selecionadas com F score
-            # mais alto até ao valor do percentile indicado (50% de 10 features equivale a 5 features)
-        )
-        idxs = np.argsort(self.F)[-self.percentile:]
-        features = np.array(dataset.features)[idxs]
-        return Dataset(X=dataset.X[:, idxs], y=dataset.y, features=list(features), label=dataset.label)
-
-    def fit(self, dataset: Dataset) -> 'SelectPercentile':
-        """
-        It fits SelectKBest to compute the F scores and p-values.
+        It fits SelectPercentile to compute the F scores and p-values.
 
         Parameters
         ----------
@@ -79,36 +63,49 @@ class SelectPercentile:
         self.F, self.p = self.score_func(dataset)
         return self
 
-    def fit_transform(self, dataset: Dataset) -> Dataset:
+
+    def _transform(self, dataset: Dataset)-> Dataset:
         """
-        Method that executes the fit method and then the transform method.
+        Select the top features based on the computed F-scores and the specified percentile.
 
         Parameters
         ----------
         dataset: Dataset
-            A labeled dataset
+            The input dataset to transform.
 
         Returns
         -------
-        dataset: Dataset
-            Dataset with the selected variables
+        transformed: Dataset
+            A new dataset containing only the selected features.
         """
-        self.fit(dataset)
-        return self.transform(dataset)
+        # Compute the number of features to select
+        num_features = int(len(dataset.features) * (self.percentile / 100))
+        num_features = max(1, num_features)  # Ensure at least one feature is selected
+
+        # Select indices of the top features
+        top_indices = np.argsort(self.F)[-num_features:]
+
+        # Subset the dataset
+        selected_X = dataset.X[:, top_indices]
+        selected_features = [dataset.features[i] for i in top_indices]
+
+        return Dataset(X=selected_X, y=dataset.y, features=selected_features, label=dataset.label)
 
 
-if __name__ == '__main__':
-    dataset = Dataset(X=np.array([[0, 2, 0, 3],
-                                  [0, 1, 4, 3],
-                                  [0, 1, 1, 3]]),
-                      y=np.array([0, 1, 0]),
-                      features=["f1", "f2", "f3", "f4"],
-                      label="y")
 
-    # dataset = read_csv('C:\Users\Bruna\PycharmProjects\pythonSIB2\SIB\src\si\IO\iris.csv', sep=",", label=True)
-    select = SelectPercentile(percentile=50)
-    # chamar o método f_classification p/ o cálculo e introduzir valor de
-    # percentile (em percentagem)
-    select = select.fit(dataset)
-    dataset = select.transform(dataset)
-    print(dataset.features)
+if __name__ == "__main__":
+    from si.data.dataset import Dataset
+
+    X = np.array([[1, 2, 3, 4],
+                  [5, 6, 7, 8],
+                  [9, 10, 11, 12]])
+    y = np.array([0, 1, 0])
+    features = ["a", "b", "c", "d"]
+    label = "target"
+
+    dataset = Dataset(X=X, y=y, features=features, label=label)
+    selector = SelectPercentile(percentile=50)
+    selector.fit(dataset)
+    transformed_dataset = selector.transform(dataset)
+
+    print("Features selecionadas:", transformed_dataset.features)
---
+++ tests/unit_tests/test_select_percentile.py
@@ -0,0 +1,30 @@
+import os
+import unittest
+from si.feature_selection.select_percentile import SelectPercentile
+from si.io.csv_file import read_csv
+from si.statistics.f_classification import f_classification
+
+class TestSelectPercentile(unittest.TestCase):
+
+    def setUp(self):
+        self.dataset_path = os.path.join('datasets', 'iris', 'iris.csv')
+        self.dataset = read_csv(filename=self.dataset_path, features=True, label=True)
+
+    def test_fit(self):
+        
+        selector = SelectPercentile(score_func=f_classification, percentile=50)
+        selector.fit(self.dataset)
+
+        self.assertTrue(selector.F.shape[0] > 0)
+        self.assertTrue(selector.p.shape[0] > 0)
+
+
+    def test_transform(self):
+        selector = SelectPercentile(score_func=f_classification, percentile=50)
+        selector.fit(self.dataset)
+        transformed_dataset = selector.transform(self.dataset)
+
+        
+        self.assertLess(len(transformed_dataset.features), len(self.dataset.features))
+        self.assertLess(transformed_dataset.X.shape[1], self.dataset.X.shape[1])
+        self.assertEqual(len(transformed_dataset.features),2)
